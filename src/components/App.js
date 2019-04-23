359cadcf01cac566764ba8fef566d47ab8e2fbe7
FleuryEscudero <fleury.ea.27@gmail.com>
2019-04-23 02:22:58 -0500

    React app de cotizador de Seguros

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import Header from './Header';
+import Formulario from './Formulario';
+
+
+
+class App extends Component {
+
+
+  cotizarSeguro = (datos)=>{
+    console.log(datos)
+  }
+  render (){
+
+    return (
+      <div className="contenedor">
+        <Header
+          titulo ='Cotizador de Seguro de Auto'/>
+          <div className="contenedor-formulario">
+            <Formulario
+            cotizarSeguro={this.cotizarSeguro}
+            />
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
